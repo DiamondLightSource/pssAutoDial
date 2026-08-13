81bdfa56459d990400a3a759c839357a412f0e08
Timothy Burd <ese89794@pc0069.cs.diamond.ac.uk>
2026-08-13 14:32:30 +0100

    Update module name in tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,9 @@
 import subprocess
 import sys
 
-from pssAutoDial import __version__
+from pss_autodial import __version__
 
 
 def test_cli_version():
-    cmd = [sys.executable, "-m", "pssAutoDial", "--version"]
+    cmd = [sys.executable, "-m", "pss_autodial", "--version"]
     assert subprocess.check_output(cmd).decode().strip() == __version__
